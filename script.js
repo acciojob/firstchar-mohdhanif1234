a836989cb3262fa31119cb57a286bd1a0630ddab
mohdhanif1234 <88095538+mohdhanif1234@users.noreply.github.com>
2024-01-25 20:17:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function firstChar(text) {
-  // your code here
+text = text.trim();
+	if(text === '') return ''
+	else return text.charAt(0);
 }
 
 // Do not change the code below
 
-const text = prompt("Enter text:");
+const text = prompt("Enter text:")
 alert(firstChar(text));
